0c04f9dec2012c171aaf055dcdcaadcb292f84e6
Lanvige Jiang <lanvige@gmail.com>
2012-09-14 14:03:33 +0800

    Add Console and config Logging

---
+++ Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System.IO;
+using Biz;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDownloadManager dm = new DownloadManager();
+
+            ICourseStructureManager cs = new CourseStructureManager(dm, 201, "development", "en", "none");
+        }
+    }
+}
